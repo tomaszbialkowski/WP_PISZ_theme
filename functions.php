bd76f9525359ae7b3fd20f910cdbf699648c6111
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2025-02-03 15:01:01 +0100

    poprawione wyswietlanie  aktywnej klasy w menu dla analitycy

--- functions.php
+++ functions.php
@@ -9,5 +9,6 @@ require_once get_template_directory() . '/inc/custom-post-types.php';
 require_once get_template_directory() . '/inc/admin-columns.php';
 require_once get_template_directory() . '/inc/custom-blocks.php';
 require_once get_template_directory() . '/inc/polylang-strings.php';
+require_once get_template_directory() . '/inc/custom-menu-classes.php';
 
 ?>
